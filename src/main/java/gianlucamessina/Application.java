68d57920318aa27c6908058774b28756c6171c31
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-08-31 17:54:59 +0200

    query per ottenere lista di partite vinte in casa e lista di partite vinte in trasferta

--- src/main/java/gianlucamessina/Application.java
+++ src/main/java/gianlucamessina/Application.java
@@ -1,27 +1,39 @@
 package gianlucamessina;
 
 
+import com.github.javafaker.Faker;
 import gianlucamessina.dao.EventoDao;
 import gianlucamessina.dao.LocationDao;
+import gianlucamessina.dao.PartitaDiCalcioDao;
+import gianlucamessina.dao.PersonaDao;
 import gianlucamessina.entities.Concerto;
 import gianlucamessina.entities.Location;
+import gianlucamessina.entities.PartitaDiCalcio;
+import gianlucamessina.entities.Persona;
 import gianlucamessina.enums.GenereConcerto;
+import gianlucamessina.enums.PersonaSesso;
 import gianlucamessina.enums.TipoEvento;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
-import java.util.UUID;
+import java.util.*;
+import java.util.function.Supplier;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BE_U1_S3_L4");
 
     public static void main(String[] args) {
+
         EntityManager em = emf.createEntityManager();
+        Random random = new Random();
+        Faker faker = new Faker(Locale.ITALY);
 
         LocationDao ld = new LocationDao(em);
         EventoDao ed = new EventoDao(em);
+        PersonaDao persDao = new PersonaDao(em);
+        PartitaDiCalcioDao partitaDao = new PartitaDiCalcioDao(em);
         //*********************** SALVATAGGIO LOCATIONS ****************************************
 
         Location sanSiroFromDb = ld.getById(UUID.fromString("329e5baf-7a97-4bb3-b560-bcafdce52999"));
@@ -40,9 +52,51 @@ public class Application {
         Concerto kanyeWest = new Concerto("Concerto di Kanye West", LocalDate.now().plusYears(1), "Prima data del tour", TipoEvento.PUBBLICO, 16000, circoMassimoFromDb, GenereConcerto.ROCK, false);
         //ed.save(kanyeWest);
 
+        Supplier<Persona> randomPersonaSupplier = () -> {
+            String nome = faker.name().firstName();
+            String cognome = faker.name().lastName();
+            String email = faker.internet().emailAddress();
+            LocalDate dataNascita = LocalDate.now().minusYears(23);
+            PersonaSesso[] sessoPersone = PersonaSesso.values();
+            PersonaSesso sesso = sessoPersone[random.nextInt(sessoPersone.length)];
+            return new Persona(nome, cognome, email, dataNascita, sesso);
+        };
+        for (int i = 0; i < 20; i++) {
+            //persDao.save(randomPersonaSupplier.get());
+        }
+
+        Supplier<PartitaDiCalcio> randomPartiteCalcioSupplier = () -> {
+            String titolo = "Partita di calcio";
+            LocalDate dataEvento = LocalDate.now().plusWeeks(random.nextInt(1, 4));
+            String descrizione = "una bella partita";
+            TipoEvento[] tipiEventi = TipoEvento.values();
+            TipoEvento tipoEv = tipiEventi[random.nextInt(tipiEventi.length)];
+            int numPartecipanti = random.nextInt(1000);
+            Location location = sanSiroFromDb;
+            String squadraCasa = faker.esports().team();
+            String squadraOspite = faker.esports().team();
+            List<String> squadre = new ArrayList<>();
+            squadre.add(squadraCasa);
+            squadre.add(squadraOspite);
+            String squadraVincente = squadre.get(random.nextInt(squadre.size()));
+            int golCasa = random.nextInt(5);
+            int golOspite = random.nextInt(5);
+            return new PartitaDiCalcio(titolo, dataEvento, descrizione, tipoEv, numPartecipanti, location, squadraCasa, squadraOspite, squadraVincente, golCasa, golOspite);
+
+        };
+
+        for (int i = 0; i < 5; i++) {
+            //partitaDao.save(randomPartiteCalcioSupplier.get());
+        }
+
         System.out.println("LISTA CONCERTI STREAMING");
         ed.getConcertiInStreaming(true).forEach(System.out::println);
         System.out.println("LISTA CONCERTI ROCK");
         ed.getConcertiPerGenere(GenereConcerto.ROCK).forEach(System.out::println);
+
+        System.out.println("LISTA PARITE VINTE IN CASA");
+        partitaDao.getPartiteVinteInCasa().forEach(System.out::println);
+        System.out.println("LISTA PARTITE VINTE IN TRASFERTA");
+        partitaDao.getPartiteVinteInTrasferta().forEach(System.out::println);
     }
 }
---
+++ src/main/java/gianlucamessina/dao/PartitaDiCalcioDao.java
@@ -0,0 +1,46 @@
+package gianlucamessina.dao;
+
+import gianlucamessina.entities.PartitaDiCalcio;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class PartitaDiCalcioDao {
+    private final EntityManager em;
+
+    public PartitaDiCalcioDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(PartitaDiCalcio partita) {
+        //NEL PROCESSO DI SCRITTURA BISOGNA UTILIZZARE UNA TRANSAZIONE PER ASSICURARSI CHE AVVENGA IN SICUREZZA
+
+        //1. chiedo all'entity manager di fornire una transazione
+        EntityTransaction transaction = em.getTransaction();
+
+        //2.avviamo la transazione
+        transaction.begin();
+
+        //3.aggiungo l'evento al persistence context
+        em.persist(partita);
+
+        //4.concludiamo la transazione salvando l'evento nel DB
+        transaction.commit();
+
+        System.out.println("La partita tra : " + partita.getSquadraDiCasa() + " e " + partita.getSquadraOspite() + " è stata salvata con successo!");
+    }
+
+    public List<PartitaDiCalcio> getPartiteVinteInCasa() {
+        TypedQuery<PartitaDiCalcio> query = em.createQuery("SELECT p FROM PartitaDiCalcio p WHERE p.numeroGolSquadraDiCasa > p.numeroGolSquadraOspite", PartitaDiCalcio.class);
+        return query.getResultList();
+    }
+
+    public List<PartitaDiCalcio> getPartiteVinteInTrasferta() {
+        TypedQuery<PartitaDiCalcio> query = em.createQuery("SELECT p FROM PartitaDiCalcio p WHERE p.numeroGolSquadraDiCasa < p.numeroGolSquadraOspite", PartitaDiCalcio.class);
+        return query.getResultList();
+    }
+
+
+}
